da43f506c00e3c339ccb31d21a0a9c5dcbfa71cc
gpageinin <gpageinin4.00@gmail.com>
2020-03-06 11:40:31 +0900

    agentのフィールド名を修正

--- agent.py
+++ agent.py
@@ -7,12 +7,12 @@ class Agent(nn.Module):
     def __init__(self, hidden):
         super().__init__()
 
-        self.fc1 = nn.Linear(4, hidden)
-        self.fc2 = nn.Linear(hidden, hidden)
-        self.fc3 = nn.Linear(hidden, 2)
+        self._fc1 = nn.Linear(4, hidden)
+        self._fc2 = nn.Linear(hidden, hidden)
+        self._fc3 = nn.Linear(hidden, 2)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        x = F.relu(self._fc1(x))
+        x = F.relu(self._fc2(x))
+        return self._fc3(x)
 
